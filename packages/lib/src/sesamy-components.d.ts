3e29d12727c957e5abb1b17d61d2f2aee7654665
Markus Ahlstrand <markus@ahlstrand.de>
2024-09-23 00:17:46 +0200

    fix: types for registration wall

--- packages/lib/src/sesamy-components.d.ts
+++ packages/lib/src/sesamy-components.d.ts
@@ -9,11 +9,16 @@ declare module "../../../dist/lib/sesamy-components.es.js" {
   export class Paywall extends HTMLElement {
     // Add properties for Paywall if needed
   }
+
+  export class RegistrationWall extends HTMLElement {
+    // Add properties for Paywall if needed
+  }
 }
 
 declare global {
   interface HTMLElementTagNameMap {
     "sesamy-login": Login;
     "sesamy-paywall": Paywall;
+    "sesamy-registration-wall": RegistrationWall;
   }
 }
