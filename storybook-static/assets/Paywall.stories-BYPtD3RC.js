2d05bd93a223a87939c4ff5887a876c3eaf95f3c
Markus Ahlstrand <markus@ahlstrand.de>
2024-09-26 13:51:09 +0200

    fix: change build script name

---
+++ storybook-static/assets/Paywall.stories-BYPtD3RC.js
@@ -0,0 +1,23 @@
+import{k as c}from"./lit-html-DZH-Jm0H.js";import"./sesamy-components.es-DcGkowaI.js";const g={title:"Components/Paywall",tags:["autodocs"],component:"sesamy-paywall",render:u=>c` <sesamy-paywall></sesamy-paywall> `,argTypes:{}},e={args:{}},a={args:{},parameters:{viewport:{defaultViewport:"mobile1"}}},r={args:{},parameters:{viewport:{defaultViewport:"tablet"}}};var s,t,o;e.parameters={...e.parameters,docs:{...(s=e.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  args: {
+    // Add any default props here if needed
+  }
+}`,...(o=(t=e.parameters)==null?void 0:t.docs)==null?void 0:o.source}}};var n,p,d;a.parameters={...a.parameters,docs:{...(n=a.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  args: {
+    // Add any props for mobile view if needed
+  },
+  parameters: {
+    viewport: {
+      defaultViewport: "mobile1"
+    }
+  }
+}`,...(d=(p=a.parameters)==null?void 0:p.docs)==null?void 0:d.source}}};var i,l,m;r.parameters={...r.parameters,docs:{...(i=r.parameters)==null?void 0:i.docs,source:{originalSource:`{
+  args: {
+    // Add any props for tablet view if needed
+  },
+  parameters: {
+    viewport: {
+      defaultViewport: "tablet"
+    }
+  }
+}`,...(m=(l=r.parameters)==null?void 0:l.docs)==null?void 0:m.source}}};const y=["Default","MobileView","TabletView"];export{e as Default,a as MobileView,r as TabletView,y as __namedExportsOrder,g as default};
