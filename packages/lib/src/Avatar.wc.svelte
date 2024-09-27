f78f6dd24cb288d7848652cdb04cb7aad16e45e7
kkortes <kim@kortes.se>
2024-09-27 19:03:02 +0200

    feat: enhance spinner and add tailwind-merge

--- packages/lib/src/Avatar.wc.svelte
+++ packages/lib/src/Avatar.wc.svelte
@@ -1,6 +1,7 @@
 <svelte:options customElement="sesamy-avatar" />
 
 <script lang="ts">
+  import { twMerge } from "tailwind-merge";
   import Base from "./Base.svelte";
 
   export let src = "";
@@ -8,68 +9,53 @@
   export let size = "md";
   export let loading = false;
 
-  const sizeClasses: Record<string, string> = {
-    sm: "w-8 h-8",
-    md: "w-12 h-12",
-    lg: "w-16 h-16",
-  };
-
-  const spinnerSizes: Record<string, string> = {
-    sm: "w-10 h-10",
-    md: "w-14 h-14",
-    lg: "w-18 h-18",
-  };
-
   export let onclick = () => {};
 </script>
 
 <Base>
-  {#if loading}
-    <svg
-      class={`absolute ${sizeClasses[size]}`}
-      viewBox="0 0 100 100"
-      xmlns="http://www.w3.org/2000/svg"
+  <div class="relative inline-block">
+    <button
+      type="button"
+      class={twMerge(
+        "relative inline-block rounded-full overflow-hidden bg-gray-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500",
+        size === "sm" && "w-8 h-8",
+        size === "md" && "w-12 h-12",
+        size === "lg" && "w-16 h-16",
+      )}
+      {onclick}
+      onkeydown={(e) => e.key === "Enter" && onclick}
+      aria-label={alt}
     >
-      <circle
-        cx="50"
-        cy="50"
-        r="45"
-        stroke-width="10"
-        stroke="#3B82F6"
-        fill="none"
-        stroke-dasharray="283"
-        stroke-dashoffset="283"
+      {#if src}
+        <img {src} {alt} class="w-full h-full object-cover" />
+      {:else}
+        <div
+          class="flex items-center justify-center w-full h-full text-gray-500"
+        >
+          {alt.charAt(0).toUpperCase()}
+        </div>
+      {/if}
+    </button>
+    {#if loading}
+      <div
+        class="absolute left-[50%] -translate-x-[50%] top-[50%] -translate-y-[50%]"
       >
-        <animateTransform
-          attributeName="transform"
-          type="rotate"
-          from="0 50 50"
-          to="360 50 50"
-          dur="1s"
-          repeatCount="indefinite"
-        />
-        <animate
-          attributeName="stroke-dashoffset"
-          values="283;0"
-          dur="1s"
-          repeatCount="indefinite"
-        />
-      </circle>
-    </svg>
-  {/if}
-  <button
-    type="button"
-    class={`relative inline-block ${sizeClasses[size]} rounded-full overflow-hidden bg-gray-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500`}
-    {onclick}
-    onkeydown={(e) => e.key === "Enter" && onclick}
-    aria-label={alt}
-  >
-    {#if src}
-      <img {src} {alt} class="w-full h-full object-cover" />
-    {:else}
-      <div class="flex items-center justify-center w-full h-full text-gray-500">
-        {alt.charAt(0).toUpperCase()}
+        <svg
+          class={twMerge(
+            "animate-spin",
+            size === "sm" && "w-7 h-7",
+            size === "md" && "w-10 h-10",
+            size === "lg" && "w-14 h-14",
+          )}
+          viewBox="0 0 125 125"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M0 62.5C0 28 28 0 62.5 0v18.8c-24.2 0-43.8 19.6-43.8 43.8H0z"
+            style="fill:black;"
+          />
+        </svg>
       </div>
     {/if}
-  </button>
+  </div>
 </Base>
