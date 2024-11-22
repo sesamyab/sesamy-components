ec24fde52a8ff0ca136fb1414120f7ab313ada96
Jonatan Schultz <jonatan@jschultz.se>
2024-11-22 15:19:55 +0100

    feat: paywall tag position

--- packages/lib/src/components/Tag.svelte
+++ packages/lib/src/components/Tag.svelte
@@ -4,12 +4,10 @@
   let { text, chunky, t }: { text: string; chunky?: boolean; t: any } = $props();
 </script>
 
-<div
-  class={twMerge('z-10 absolute top-1 right-8 @md:right-5 rotate-6', chunky && 'right-12 top-3')}
->
+<div class={twMerge('absolute top-2 -right-3 rotate-3', chunky && 'top-5')}>
   <div
     class={twMerge(
-      'absolute bg-primary bottom-0 rounded left-1/2 -translate-x-1/2 px-2 text-white text-xs py-1 font-bold whitespace-nowrap',
+      'absolute bg-primary bottom-0 rounded right-0 px-2 text-white text-xs py-1 font-bold whitespace-nowrap',
       chunky && 'px-3 text-sm @xl:text-base bottom-1'
     )}
   >
