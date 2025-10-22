3f871614e7078c90ddd5a7a6d4c74154cf61b0a4
Jonatan Schultz <jonatan@jschultz.se>
2025-10-22 21:11:25 +0200

    chore: review notes

--- packages/lib/src/components/paywall/Renderer.svelte
+++ packages/lib/src/components/paywall/Renderer.svelte
@@ -174,6 +174,7 @@
       --s-primary-color: var(--sesamy-paywall-primary-color, ${mainColor});
       --s-paywall-bg-color: var(--sesamy-paywall-bg-color, ${paywallBgColor});
       --s-paywall-text-color: var(--sesamy-paywall-text-color, ${paywallTextColor});
+      --s-paywall-text-color-70: var(--s-paywall-text-color);
       --s-paywall-text-color-70: color-mix(in srgb, var(--s-paywall-text-color) 70%, transparent);
       --s-paywall-border-radius: var(--sesamy-paywall-border-radius, 0.5rem);
       --s-paywall-border-radius-desktop: var(--sesamy-paywall-border-radius-desktop, calc(var(--s-paywall-border-radius) * 3));
